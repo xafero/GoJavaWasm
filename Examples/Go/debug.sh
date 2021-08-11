e6517cb1e19415de2c5d49f2e69329e370787628
xafero <gberendt@outlook.com>
2021-08-11 20:10:07 +0200

    Removed native part

--- Examples/Go/debug.sh
+++ Examples/Go/debug.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-echo " ::: Run natively..."
-go run main.go
-echo " ::: Compiling to WASM..."
-GOOS=js GOARCH=wasm go build -o main.wasm
-echo " ::: Run with java..."
 java \
    -Dorg.slf4j.simpleLogger.log.io.github.kawamuray.wasmtime.Func=info \
    -Dorg.slf4j.simpleLogger.log.io.github.kawamuray.wasmtime.NativeLibraryLoader=info \
@@ -11,4 +6,3 @@ java \
    -Dorg.slf4j.simpleLogger.showThreadName=false \
    -Dorg.slf4j.simpleLogger.showLogName=false \
    -jar ../../../GoJavaWasm/wasm-exec/target/wasm-exec-1.0-SNAPSHOT.jar ./main.wasm
-echo " ::: Done."
